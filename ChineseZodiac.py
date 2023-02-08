75be22462b896dcfcd363d1f67c86bfc76193c80
Diseloca <diselocagaming@gmail.com>
2023-02-08 12:32:17 +0100

    update

--- ChineseZodiac.py
+++ ChineseZodiac.py
@@ -1,19 +1,7 @@
-meses = {
-    0: "mono", 1: "gallo", 2: "perro", 3: "cerdo",
-    4: "rata", 5: "buey", 6: "tigre", 7: "conejo",
-    8: "dragon", 9: "serpiente", 10: "caballo", 11: "cabra"
+SIGNS = {
+    0: "monkey", 1: "rooster", 2: "dog", 3: "pig",
+    4: "rat", 5: "ox", 6: "tiger", 7: "rabbit",
+    8: "dragon", 9: "snake", 10: "horse", 11: "goat"
 }
 
-
-def getsign(year, d):
-    n = year % 12
-    return d.get(n)
-
-
-def main():
-    year = int(input("Introduce tu fecha de nacimineto [YYYY]: "))
-    sign = getsign(year, meses)
-    print("Tu signo es:", sign)
-
-
-main()
+print("Your sign is:", SIGNS.get(int(input("Introduce your birth year [YYYY]: ")) % 12))
