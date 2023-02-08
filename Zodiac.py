383401cd68c658f681e853453ae9a65c6c88e8b3
Diseloca <diselocagaming@gmail.com>
2023-02-08 12:25:13 +0100

    update

--- Zodiac.py
+++ Zodiac.py
@@ -1,5 +1,5 @@
-M = ["january", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre",
-     "noviembre", "diciembre"]
+M = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october",
+     "november", "december"]
 
 
 def validate_input(day, month):
@@ -12,37 +12,37 @@ def validate_input(day, month):
         exit(0)
 
     # months with 30 days
-    if (month in ("abril", "junio", "septiembre", "noviembre")) and day > 30:
+    if (month in ("april", "june", "september", "november")) and day > 30:
         print(month, "has not day", day)
         exit(0)
 
-    if month == "febrero" and day > 29:
-        print("Error dia de febrero mayor que 29")
+    if month == "february" and day > 29:
+        print("Error dia de february mayr que 29")
         exit(0)
 
 
 def get_sign(day, month):
-    if (month == "january" and day >= 21) or (month == "febrero" and day <= 19):
+    if (month == "january" and day >= 21) or (month == "february" and day <= 19):
         sign = "Acuario"
-    elif month == "febrero" or (month == "marzo" and day <= 20):
+    elif month == "february" or (month == "march" and day <= 20):
         sign = "Piscis"
-    elif month == "marzo" or (month == "abril" and day <= 20):
+    elif month == "march" or (month == "april" and day <= 20):
         sign = "Aries"
-    elif month == "abril" or (month == "mayo" and day <= 20):
+    elif month == "april" or (month == "may" and day <= 20):
         sign = "Tauro"
-    elif month == "mayo" or (month == "junio" and day <= 21):
+    elif month == "may" or (month == "june" and day <= 21):
         sign = "Geminis"
-    elif month == "junio" or (month == "julio" and day <= 22):
+    elif month == "june" or (month == "july" and day <= 22):
         sign = "Cancer"
-    elif month == "julio" or (month == "agosto" and day <= 23):
+    elif month == "july" or (month == "august" and day <= 23):
         sign = "Leo"
-    elif month == "agosto" or (month == "septiembre" and day <= 22):
+    elif month == "august" or (month == "september" and day <= 22):
         sign = "Virgo"
-    elif month == "septiembre" or (month == "octubre" and day <= 22):
+    elif month == "september" or (month == "october" and day <= 22):
         sign = "Libra"
-    elif month == "octubre" or (month == "noviembre" and day <= 22):
+    elif month == "october" or (month == "november" and day <= 22):
         sign = "Escorpio"
-    elif month == "noviembre" or (month == "diciembre" and day <= 21):
+    elif month == "november" or (month == "december" and day <= 21):
         sign = "Sagitario"
     else:
         sign = "Capricornio"
